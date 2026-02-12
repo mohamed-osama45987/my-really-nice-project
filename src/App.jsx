39daec094281a36cd3c6f326846e1f221b46666d
mohamed-osama45987 <mo.mohamed45987@gmail.com>
2026-02-12 17:42:59 +0200

    changed title

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Mohamed</h1>
+      <h1>Ahmed</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
